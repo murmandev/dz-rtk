d50b5e1e86bf657a1070fb519370d2b78588e4ab
Novokshonov Ivan <zatoexp@gmail.com>
2023-11-13 17:39:32 +0300

    Добавил загрузку картинок, переделал like и возврт

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import Index, DetailArticleView, LikeArticle
 
 urlpatterns = [
@@ -9,3 +11,5 @@ urlpatterns = [
     path('<int:pk>/like', LikeArticle.as_view(), name='like_article'),
 
 ]
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- dznews/settings.py
+++ dznews/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'tinymce',
+
 ]
 
 MIDDLEWARE = [
@@ -129,6 +131,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+# Дописано
+MEDIA_URL = '/media/' 
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/') 
 
 LOGIN_REDIRECT_URL = 'index'
 
