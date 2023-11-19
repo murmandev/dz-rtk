1814568f7642e4f13a527c0881034a60eddcb8ea
”Ivan <murmandev@yandex.ru>
2023-11-19 10:37:27 +0300

    Сделал профиль и убрал ошибку валидации формы

--- users/views.py
+++ users/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from .forms import UserRegisterForm
-from django.views.generic import DetailView
-from django.contrib.auth.models import User
 
 class RegisterView(View):
     def get(self, request):
@@ -12,8 +10,16 @@ class RegisterView(View):
     def post(self, request):
         form = UserRegisterForm(request.POST)
         
-    
         if form.is_valid():
-            form.save()
+            user = form.save(commit=False)  # создание объекта без сохранения в БД
+            user.save()
             return redirect('login')
-        
+        else:
+            form = UserRegisterForm()
+            return render(request, 'users/register.html', {'form': form})
+    
+       
+
+class ProfileView(View):
+    def get(self, request):
+        return render(request, 'users/profile.html')
