4891bf2840b4d0e7cf040617892d01a89b1f1b62
”Ivan <murmandev@yandex.ru>
2023-12-04 20:23:41 +0300

    Название полей и стили в добавить новость

--- core/models.py
+++ core/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 
 
 class Article(models.Model):
-    title       = models.CharField(max_length=255)
-    content     = HTMLField()
-    date        = models.DateField(auto_now_add=True)
-    author      = models.ForeignKey(User, on_delete=models.CASCADE)
+    title       = models.CharField(max_length=255, verbose_name="Заголовок")
+    content     = HTMLField(verbose_name="Текст")
+    date        = models.DateField(auto_now_add=True, verbose_name="Дата")
+    author      = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор")
     featured    = models.BooleanField(default=False)
     likes       = models.ManyToManyField(User, related_name='likes', blank=True)
-    image       = models.ImageField(upload_to='images/')
+    image       = models.ImageField(upload_to='images/', verbose_name="Изображение")
