edf4a047a5f5c07088631f00199fb2f639c68e09
”Ivan <murmandev@yandex.ru>
2023-12-10 13:08:03 +0300

    Оповещения на форме регистрации

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from .forms import UserRegisterForm
+from django.contrib import messages
+
 
 class RegisterView(View):
 
@@ -10,13 +12,14 @@ class RegisterView(View):
 
     def post(self, request):
         form = UserRegisterForm(request.POST)
-
+        messages.success(request, UserRegisterForm.error_messages)
         if form.is_valid():
             user = form.save(commit=False)  # создание объекта без сохранения в БД
             user.save()
             return redirect('login')
         else:
             form = UserRegisterForm()
+            print(UserRegisterForm.error_messages)
             return render(request, 'users/register.html', {'form': form})
 
 
