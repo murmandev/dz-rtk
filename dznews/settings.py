d23ba0253ec9872604fc371caf0491a7507f7432
”Ivan <murmandev@yandex.ru>
2023-12-23 10:04:18 +0300

    Правки настроек

--- dznews/settings.py
+++ dznews/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIR = (
     os.path.join(BASE_DIR, 'static'),
 )
@@ -138,7 +138,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Дописано
 MEDIA_URL = '/media/' 
 
-MEDIA_ROOT = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 
 
