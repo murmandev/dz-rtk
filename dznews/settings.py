d32b0cade2acf63806d75856b26601004c63b60a
”Ivan <murmandev@yandex.ru>
2023-12-23 20:47:22 +0300

    Delete postgre server connect

--- dznews/settings.py
+++ dznews/settings.py
@@ -88,23 +88,14 @@ WSGI_APPLICATION = 'dznews.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dzrtk',
-        'USER': 'admin',
-        'PASSWORD': 'Ad310380',
-        'HOST': '195.2.85.141',
-        'PORT': '5432',
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
